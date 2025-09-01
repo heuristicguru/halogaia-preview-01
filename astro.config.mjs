070f19bb2b48ea5cd7b4c7ed23687d19cdb17002
heuristicguru <128498745+heuristicguru@users.noreply.github.com>
2025-09-01 13:52:56 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
-// Set these to your GitHub Pages details before deploying
 export default defineConfig({
-  site: 'https://example.github.io/your-repo',
-  base: '/your-repo/',
+  site: 'https://heuristicguru.github.io/halogaia-preview-01',
+  base: '/halogaia-preview-01/',
   integrations: [tailwind(), sitemap()],
   output: 'static'
 });
