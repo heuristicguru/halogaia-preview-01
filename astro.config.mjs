c899825572a191d4f1e18ee0d4b9b73dad46a199
heuristicguru <128498745+heuristicguru@users.noreply.github.com>
2025-09-01 13:06:44 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://heuristicguru.github.io/halogaia-preview-01',
+  base: '/halogaia-preview-01/',   // 👈 must exactly match your repo name
   integrations: [tailwind(), sitemap()],
   output: 'static'
 });
